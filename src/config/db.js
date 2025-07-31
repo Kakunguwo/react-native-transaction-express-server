f45579482216cd38a95a9f73bf70945c18bdd970
Ronnie Kakunguwo <ronniekakunguwo03@gmail.com>
2025-07-31 16:32:21 +0200

    Refactor database initialization logic and update import statements

--- src/config/db.js
+++ src/config/db.js
@@ -4,3 +4,21 @@ import "dotenv/config";
 
 // This creates a sql connection using our database
 export const sql = neon(process.env.DATABASE_URL);
+
+export async function initializeDatabase() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10,2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+
+    console.log("Database initialised successfully");
+  } catch (error) {
+    console.log("Error initialising database", error);
+    process.exit(1);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
-import { sql } from "./config/db.js";
+import { initializeDatabase } from "./config/db.js";
 
 dotenv.config({
   path: "./.env",
 });
 
 const PORT = process.env.PORT;
-
-async function initializeDatabase() {
-  try {
-    await sql`CREATE TABLE IF NOT EXISTS transactions(
-        id SERIAL PRIMARY KEY,
-        user_id VARCHAR(255) NOT NULL,
-        title VARCHAR(255) NOT NULL,
-        amount DECIMAL(10,2) NOT NULL,
-        category VARCHAR(255) NOT NULL,
-        created_at DATE NOT NULL DEFAULT CURRENT_DATE
-    )`;
-
-    console.log("Database initialised successfully");
-  } catch (error) {
-    console.log("Error initialising database", error);
-    process.exit(1);
-  }
-}
 
 initializeDatabase().then(() => {
   app.listen(PORT, () => {
