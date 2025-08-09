af5d9180a22f4c1f41fb1c8827b4045a682b55dc
Ronnie Kakunguwo <ronniekakunguwo03@gmail.com>
2025-08-09 05:02:01 +0200

    Refactor health route definition to use route chaining for improved readability

--- src/routes/health.js
+++ src/routes/health.js
@@ -2,8 +2,8 @@ import { Router } from "express";
 
 import { checkHealth } from "../controllers/health.js";
 
-router.get("/", checkHealth);
-
 const router = Router();
+
+router.route("/").get(checkHealth);
 
 export default router;
